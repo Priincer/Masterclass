fb203d0d2d12c19e701bad731c31d96e0a0ec7ac
priincer7 <redlmarkus@gmx.at>
2025-11-28 14:26:56 +0100

    init auth-service project with Spring Boot, Maven, and initial setup

---
+++ backend/auth-service/src/test/java/com/masterclass/auth/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masterclass.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
